0ea949c1df8394f09d1f930e7fadbc3dbe192b14
Miller Correa <macorreag@unal.edu.co>
2018-05-20 02:52:47 -0500

    [working] in Barchart responsive

--- js/index.js
+++ js/index.js
@@ -12,6 +12,8 @@ const SHAPECD = "https://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/servi
 features[i].properties.BoroCD  = 100 * BOROUGH + CD
 features[i].geometry.coordinates[j] = Coordinates of Shape
 */
+const HOUSEDATA = "https://data.cityofnewyork.us/api/views/hg8x-zxpr/rows.json?accessType=DOWNLOAD";
+
 const BOROUGH = ["Manhattan", "Bronx", "Brooklyn", "Queens", "Staten Island"];
 
 /*(Joint of Interest Areas) Not living posible Areas*/
@@ -28,10 +30,11 @@ var borough=[];
 
 /*Variables for GMaps*/
 var map;
-
+var shapeActive;
 /*ranking variables*/
 var maxDistance = 0;
-var preferenceDistance ;
+var preferenceDistance = false;
+var preferencePrice = false;
 /*Community District Sorted By distance between neighborhoods to NYU*/
 var filteredCD = [];
 
@@ -124,7 +127,12 @@ function getDataShapeDistric(){
 				subShape,
 				multiPolygon /*Is a multipolygon*/
 			);
+			communityDistrict.incomeUnits = [,,,,,];
+			communityDistrict.bedroomUnits = new Array(0, 0, 0);
 			/*SortByCommunityDistrict*/
+			if(communityDistrict.habitable){
+				filteredCD.push(communityDistrict);
+			}
 			geoCD[numberBorough(responseJSON.features[i].properties.BoroCD)].push(communityDistrict);
 		}
 
@@ -146,6 +154,8 @@ function setMarker(image,coordinates,textHover) {
 		/*Text show in event Hover*/
 		zIndex: 100
 	});
+	marker.setMap(map);
+	return marker;
 }
 
 function initMap() {
@@ -171,53 +181,33 @@ function pointInPath( point , coordLimits){
 	true:
 	false;
 }
-function drawNeigh(array){
-	for( var i = 0 ;i <array.length ; i++){
-		var marker = new google.maps.Marker({
-			position: array[i].coorCenter,
-			map: map,
-			title: array[i].name
-		});
-	}
-}
-
-
-/*/Clean Code/Clean Code/Clean Code/Clean Code/Clean Code/Clean Code/Clean Code/Clean Code*/
 
 
 class Neighborhood {
 	/*In the communityDistrict*/
-	constructor(name,coorCenter,district,coordLimits) {
+	constructor(name,coorCenter,coordLimits) {
 		this._name = name;
 		this._coorCenter = coordinateGmaps(coorCenter);
-		this._district = district;
-		this._coordLimits = coordLimits;
-		this.distanceWalk  ,
-		this.distanceCar  = [],
-		this.distanceTransport  ,
-		this.timeWalk  ,
-		this.timeCar  ,
-		this.timeTrans  ,
-		this._color = "#111111"; /*Dejar ramdom dentro del costructor para luego usar*/
+		this.distanceCar  = []
 	}
 	get name() {
 		return this._name;
 	}
-
 	get coorCenter(){
 		return this._coorCenter;
 	}
-
-	get district(){
-		return this._district;
+	draw(){
+		var image = {
+			url: 'https://i.imgur.com/PQwY47e.png?1',
+			size: new google.maps.Size(32, 32),
+			origin: new google.maps.Point(0, 0),
+			anchor: new google.maps.Point(15.5,15.5)
+		};
+		var marker = setMarker(image,this.coorCenter,this.name);
+		return marker;
 	}
-
-	get coordLimits(){
-		return this._coordLimits;
-	}
-
-
 }
+
 class CommunityDistrict {
 	/*CommunityDistrict in the 5 district */
 	constructor(num,coordLimits,multiPol){
@@ -225,6 +215,26 @@ class CommunityDistrict {
 		this._coordLimits = coordLimits;
 		this._neighborhoods = [];
 		this._multiPolygon = multiPol;
+		this._bedroomUnits = new Array(
+			/*Number of Bedroom by unit*/
+			{"value":0 ,"text":"0"},
+			{"value":0 ,"text":"1"},
+			{"value":0 ,"text":"2"},
+			{"value":0 ,"text":"3"},
+			{"value":0 ,"text":"4"},
+			{"value":0 ,"text":"5"},
+			{"value":0 ,"text":"6"},
+			{"value":0 ,"text":"Undefined"}),
+		this._incomeUnits = new Array(
+			/*Level of IncomeUnits*/
+				{"value":0 ,"text":"Extremely low IncomeUnits"},
+				{"value":0 ,"text":"Very low IncomeUnits"},
+				{"value":0 ,"text":"Low IncomeUnits"},
+				{"value":0 ,"text":"Moderate low IncomeUnits"},
+				{"value":0 ,"text":"Middle IncomeUnits"},
+				{"value":0 ,"text":"Other IncomeUnits"});
+		this.affordableUnits = 0,
+		this.numberUnits = 0;
 		if((num-(numberBorough(num)+1)*100) < 26){
 			/*JIA is a different color for the user*/
 			this._color = randomColorRGBA(0.6); /*Problema colores muy similares*/
@@ -259,6 +269,29 @@ class CommunityDistrict {
 	get color(){
 		return this._color;
 	}
+	get incomeUnits(){
+		return this._incomeUnits;
+	}
+	get bedroomUnits(){
+		return this._bedroomUnits;
+	}
+	draw(){
+		let communityDistr = new google.maps.Polygon({
+			paths: this._coordLimits,
+			strokeColor: this._color,
+			strokeWeight: 2,
+			fillColor: this._color
+		});
+		communityDistr.setMap(map);
+		return communityDistr;
+	}
+	drawNB(){
+		var neighborhoodMarkers = [];
+		for( var i = 0 ;i < this._neighborhoods.length ; i++){
+			neighborhoodMarkers.push(this._neighborhoods[i].draw());
+		}
+		return neighborhoodMarkers;
+	}
 
 }
 
@@ -275,6 +308,7 @@ function getDataNeighborhood(){
 			);
 			var numberB= BOROUGH.indexOf(data.responseJSON.data[i][16]);
 			neighborhoodToCD(neighborhood,numberB);
+
 		}
 	})
 	.fail(function(error){
@@ -282,113 +316,47 @@ function getDataNeighborhood(){
 	})
 }
 
-function compareById(a,b) {
-	if (a.id < b.id)
-	return -1;
-	if (a.id > b.id)
-	return 1;
-	return 0;
-}
+function getHousingData(){
+	/*Como parametro podria tener la URL */
+	var data = $.get(HOUSEDATA,function(){})
+	.done(function(){
+
+		var responseJSON = JSON.parse(data.responseText);
+
+		for(var i = 0 ;i < 	responseJSON.data.length ;i++){
+			let numberBoro = BOROUGH.indexOf(responseJSON.data[i][15]);
+			let numberCD = Number(responseJSON.data[i][19].slice(-2)); /*Extract last 2 items*/
+			var communityDistr = borough[numberBoro].filter(function( obj ) {
+				if(obj.id == ((numberBoro+1)*100)+numberCD){
+						return obj;
+				}
+			});
+			communityDistr[0].incomeUnits[0].value += Number(responseJSON.data[i][31]);
+			communityDistr[0].incomeUnits[1].value += Number(responseJSON.data[i][32]);
+			communityDistr[0].incomeUnits[2].value += Number(responseJSON.data[i][33]);
+			communityDistr[0].incomeUnits[3].value += Number(responseJSON.data[i][34]);
+			communityDistr[0].incomeUnits[4].value += Number(responseJSON.data[i][35]);
+			communityDistr[0].incomeUnits[5].value += Number(responseJSON.data[i][36]);
+
+			communityDistr[0].bedroomUnits[0].value += Number(responseJSON.data[i][37]);
+			communityDistr[0].bedroomUnits[1].value += Number(responseJSON.data[i][38]);
+			communityDistr[0].bedroomUnits[2].value += Number(responseJSON.data[i][39]);
+			communityDistr[0].bedroomUnits[3].value += Number(responseJSON.data[i][40]);
+			communityDistr[0].bedroomUnits[4].value += Number(responseJSON.data[i][41]);
+			communityDistr[0].bedroomUnits[5].value += Number(responseJSON.data[i][42]);
+			communityDistr[0].bedroomUnits[6].value += Number(responseJSON.data[i][43]);
+			communityDistr[0].bedroomUnits[7].value += Number(responseJSON.data[i][44]);
+
+			communityDistr[0].affordableUnits += Number(responseJSON.data[i][45]);
+			communityDistr[0].numberUnits += Number(responseJSON.data[i][47]);
 
 
-
-
-
-class BarChart {
-	constructor(data,maxX,nameClass) {
-		this._data = data;
-		this._maxX = maxX;
-		this._nameClass = nameClass;
-		/*Ancho de las barras determinada por escala va desde 0[CERO TEMPORAL EL MINIMO DEBE CAMBIAR] hasta ---*/
-
-	}
-	get data() {
-		return this._data;
-	}
-	get maxX() {
-		return this._behavior;
-	}
-	get nameClass(){
-		return this._nameClass;
-	}
-
-	get escalaX(){
-		return this._escalaX;
-	}
-	graficar(){
-		/*Toma del DOM la clase barras y todos los divs que tiene adentro ,luego con data va añadiendo div POR CADA DATO*/
-
-		var escalaX = d3.scale.linear().domain([0,d3.max(this._data)]).range([0,this._maxX]);
-		var array = d3.select(this.nameClass);
-		var test = array.select("div");
-		//test.style("background-color"," red");
-		//d3.select(this.nameClass).selector([5]).style("background-color"," red");
-		//d3.select(this.nameClass).filter(3).style("background-color","blue");
-		var even =  array.select( function ( d , i ) { return i &  1  ?  this  :  null ;});
-		/* alert("aqui esta el problema no puedo dividir la seleccion y por ende no puedo modificar directamente las caracteristicas de una sola division esto se debe realizar en cada swap la otra forma seria dibujar de nuevo todo el arreglo pero se sobrecargara el DOM");*/
-
-		//var test2 = array.selectAll("div",3);
-		//test2.style("background-color"," blue");
-		//test.
-		//var b =  d3 . selectAll ( " p " ). select ( " b " );
-		//alert(test);
-		//alert(test);
-		d3.select(this.nameClass).selectAll("div").data(this.data).enter().append('div').style("width",function(d){
-			/*Retorna un valor para el estilo en pixeles dentro del rango definido en escala y el parametro d corresponde al dato del arreglo*/
-			return d +"%";
-
-		})
-		.text(function(d){
-			return d;
-		})
-	}
-
-}
-
-
-$("document").ready(function(){
-	borough = getDataShapeDistric();
-	setTimeout(getDataNeighborhood,1000);
-
-	var  mata = [3,42,100,32,5];
-	/*Instanciando la clase*/
-	var barrasTest =  new BarChart(mata,300,".test");
-
-	barrasTest.graficar();
-
-});
-
-function updateTable(){
-
-	/*SORT BY ACTIVE PARAMETERS*/
-	$('#table').bootstrapTable({
-
-		data:filteredCD,
-		onClickRow: function (row,$element){
-			console.log(row);
-			$element.css({backgroundColor: row.color});
-			drawCD(row);
-		},
-
-		clickToSelect:true,
-		//onlyInfoPagination:true,
-		//rememberOrder:true,
-		pagination:true,
-		checkbox: true,
-		onCheck: function(row,$element){
-
-			alert("hi");
 		}
-
-
-	});
+})
+.fail(function(error){
+	console.error(error);
+})
 }
-/*FOR TEST
-*/
-
-
-
-
 function calculateDistanceCar( communityD ){
 	var distanceMatrixService = new google.maps.DistanceMatrixService;
 	var neighborhoodsDis = communityD.neighborhoods.map(a => a.coorCenter);
@@ -420,10 +388,92 @@ function calculateDistanceCar( communityD ){
 					resolve('resolved');
 				}
 			})
-
 		}, 350);
-
 	})
+}
+
+function compareById(a,b) {
+	if (a.id < b.id)
+	return -1;
+	if (a.id > b.id)
+	return 1;
+	return 0;
+}
+
+/*When Page charge Load Basic data */
+$("document").ready(function(){
+	borough = getDataShapeDistric();
+	setTimeout(getDataNeighborhood,1000);
+});
+
+/*For bootstrap -Table*/
+function runningFormatter(value, row, index) {
+	if( index == 0 ){
+		return "Winner";
+	}
+	if( index == 1 ){
+		return "Second";
+	}
+	if( index == 2 ){
+		return "Third";
+	}
+    return index + 1;
+}
+/*/Clean Code/Clean Code/Clean Code/Clean Code/Clean Code/Clean Code/Clean Code/Clean Code*/
+
+
+
+
+function updateTable(){
+
+	/*SORT BY ACTIVE PARAMETERS*/
+	$('#table').bootstrapTable({
+		data:filteredCD,
+		onClickRow: function (row,$element){
+			if(typeof(shapeActive) == "object" && typeof(neigMarkActive) == "object"){
+				shapeActive.setMap(null);
+				for(var i = 0 ;i < neigMarkActive.length ; i++){
+					neigMarkActive[i].setMap(null);
+				}
+			};
+			console.log(row);
+			$element.css({backgroundColor: row.color});
+			shapeActive = row.draw();
+			neigMarkActive = row.drawNB();
+			drawBars(row.bedroomUnits);
+
+
+		},
+		//pageList:false,
+
+		clickToSelect:true,
+		//onlyInfoPagination:true,
+		//rememberOrder:true,
+		//pagination:true,
+		checkbox: true,
+		onCheck: function(row,$element){
+
+			alert("hi");
+		}
+	});
+	$('#table').bootstrapTable('updateRow', {index:0});
+}
+/*FOR TEST
+*/
+
+
+
+
+
+async function calculateDistances(){
+	for(var i = 0;i < borough.length ;i++ ){
+		for (var j = 0;j <borough[i].length ;j++){
+			if(borough[i][j].habitable){
+				await calculateDistanceCar(borough[i][j]);
+			}
+		}
+		controlCharge(i*25);
+	}
 }
 function compareByDistances(a,b) {
 	if (a.distanceCar < b.distanceCar)
@@ -432,16 +482,63 @@ function compareByDistances(a,b) {
 	return 1;
 	return 0;
 }
-function drawCD( communityD ){
-	var nbBoundaries = new google.maps.Polygon({
-		paths: communityD.coordLimits,
-		strokeColor: communityD.color,
-		strokeWeight: 2,
-		fillColor: communityD.color
-	});
-	drawNeigh(communityD.neighborhoods);
-	nbBoundaries.setMap(map);
+function pointsPrice(communityD){
+	var acumulate = 0;
+	for(var i = 1;i < 8; i++){
+		/*priorize number of bedroom per unit*/
+			acumulate += (communityD.bedroomUnits[i].value*i);
+			/*priorize by i more bredrom more points
+			units without rooms do not have points
+			more points is best*/
+	}
+	return acumulate/communityD.numberUnits;
 }
+function pointsIncome(communityD){
+	var acumulate = 0;
+	for(var i = 1, priorize = 5;i < 5; i++,priorize--){
+		/*priorize number of bedroom per unit*/
+			acumulate += (communityD.incomeUnits[i].value*priorize);
+		/*priorize units by incomeUnits
+		Units with unknown income values are not counted
+		more points is best
+		*/
+	}
+	return acumulate/communityD.numberUnits;
+}
+function percentageByPreferences(){
+	var preferences = 0;
+	if(preferenceDistance){
+		preferences++;
+	}
+	if(preferencePrice){
+		preferences++;
+	}
+	return 100/(100*preferences);
+}
+function calculatePoints(a,b) {
+	var scoreA = 0;
+	var scoreB = 0;
+	if(preferencePrice){
+		/*Function of points priorize
+		*Number of bedroomUnits
+		*/
+		scoreA -= (pointsIncome(a) + pointsPrice(a));
+		scoreB -= (pointsIncome(b) + pointsPrice(b));
+		scoreA = scoreA*percentageByPreferences();
+		scoreB = scoreB*percentageByPreferences();
+	}
+	if(preferenceDistance){
+		scoreA += a.distanceCar*percentageByPreferences();
+		scoreB += b.distanceCar*percentageByPreferences();
+	}
+
+	if (scoreA < scoreB)
+	return -1;
+	if (scoreA > scoreB)
+	return 1;
+	return 0;
+}
+
 function controlCharge(status){
 	$('.progress-bar').text( status+"% Loading" );
 	$('.progress-bar').css( "width",status+"%" );
@@ -455,25 +552,33 @@ function controlCharge(status){
 		$('.progress-bar').removeClass("bg-success");
 	}
 }
-async function calculateDistances(){
-	for(var i = 0;i < borough.length ;i++ ){
-		for (var j = 0;j <borough[i].length ;j++){
-			if(borough[i][j].habitable){
-				filteredCD.push(borough[i][j]);
-				await calculateDistanceCar(borough[i][j]);
-			}
-		}
-		controlCharge(i*25);
-	}
-}
-async function sortByDistance(){
+
+async function calculateDistance(){
 	$('#ModalDistance').modal('toggle');
 	$('#distance').addClass("btn-danger");
 	$('#distance').prop('disabled', true);
 	await calculateDistances();
 	$('#distance').prop('disabled', false);
 	$('#distance').removeClass("btn-danger");
-	document.getElementById("distance").setAttribute('onclick','sortByPreferences()')
+	document.getElementById("distance").setAttribute('onclick','sortByDistance()')
+	sortByDistance();
+}
+function sortByDistance(){
+	preferenceDistance = pressButton($('#distance'));
+	sortByPreferences();
+}
+async function calculatePricing(){
+	//$('#ModalDistance').modal('toggle');
+	$('#price').addClass("btn-danger");
+	$('#price').prop('disabled', true);
+	await getHousingData();
+	$('#price').prop('disabled', false);
+	$('#price').removeClass("btn-danger");
+	document.getElementById("price").setAttribute('onclick','sortByPrice()');
+	sortByPrice();
+}
+function sortByPrice(){
+	preferencePrice = pressButton($('#price'));
 	sortByPreferences();
 }
 function pressButton( button ){
@@ -486,8 +591,70 @@ function pressButton( button ){
 	}
 }
 function sortByPreferences(){
-	preferenceDistance = pressButton($('#distance'));
-	filteredCD.sort(compareByDistances);
+	filteredCD.sort(calculatePoints);
 	updateTable();
-	//$('#table').bootstrapTable('updateRow',0);
 }
+
+
+
+
+function drawBars(values){
+
+var axiXSpace = 40;
+var svg = d3.select("#barChart");
+var width = svg.attr("width");
+var height = svg.attr("height")  - axiXSpace;
+var sizeFont = 15;
+
+var footerText = "Number of bedroom by units";
+
+var x = d3.scaleBand().rangeRound([0, width]).padding(0.05);
+var y = d3.scaleLinear().rangeRound([height, 0]);
+
+var g = svg.append("g");
+var tooltip = d3.select("body").append("div").attr("class", "toolTip");
+
+x.domain(values.map(function(d) { return d.text; }));
+y.domain([0, d3.max(values, function(d) { return d.value; })]);
+
+ g.selectAll(".bar")
+    .data(values)
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.text); })
+      .attr("y", function(d) { return y(d.value); })
+      .attr("width", x.bandwidth())
+      .attr("height", function(d) { return height - y(d.value); })
+			.on("mousemove", function(d){
+				 tooltip
+					 .style("left", d3.event.pageX - 50 + "px")
+					 .style("top" , d3.event.pageY - 70 + "px")
+					 .style("display", "inline-block")
+					 .html((d.text) + "<br>"  + (d.value));
+		 })
+		 .on("mouseout", function(d){ tooltip.style("display", "none")})
+		 .on("mouseover", function() { tooltip.style("display", null);});
+
+
+  g.append("g").attr("class", "axis-x")
+      .attr("transform", "translate(0,"+ (height + 1) + ")")
+      .call(d3.axisBottom(x));
+
+  svg.append("text")
+      .attr("transform",
+            "translate(" + (width/2) + " ," +
+                           ((height )+ sizeFont + 20) + ")")
+			.style("font-size",sizeFont + "px")
+      .style("text-anchor", "middle")
+      .text(footerText);
+
+}
+
+var chart = $("#barChart"),
+    aspect = chart.width()/chart.height(),
+    container = chart.parent();
+$(window).on("resize", function() {
+    var targetWidth = container.width();
+    chart.attr("width", targetWidth);
+    chart.attr("height", Math.round(targetWidth / aspect));
+}).trigger("resize");
